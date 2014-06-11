d96494318495501488b0c843dcff185d3678a568
Vikyath Harekal <harekal.vikyath@gmail.com>
2014-06-11 23:17:53 +0530

    student-styles-separated

--- student/retrieve.php
+++ student/retrieve.php
@@ -1,21 +1,6 @@
 <html>
 <head>
-<style type="text/css">
-      body{background:url(bb.jpg);
-	   background-size:200% 200%;
-	   -moz-background-size:100% 100%; /* Firefox 3.6 */
-	   background-repeat:no-repeat;
-	   word-wrap:break-word;}
-     
-      div{background:url(02.jpg);
-	  border:2px solid #a1a1a1;
-      	  padding:10px 40px; 
-          word-wrap:break-word;
-          width:1200px;
-          border-radius:25px;}
-
-     table{background:url(02.jpg);}
-  </style>
+<link rel="stylesheet" type="text/css" href="../css/retrieve.css">
 </head>
 
 <body>
