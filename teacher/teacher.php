f0a9b2360356c0aa960f995f66bef452796fb055
Vikyath Harekal <harekal.vikyath@gmail.com>
2014-06-18 07:58:41 +0530

    teacher-subject-choose-bootstrapped

--- teacher/teacher.php
+++ teacher/teacher.php
@@ -1,38 +1,49 @@
-<html>
+<!DOCTYPE html>
+<!-- saved from url=(0038)http://getbootstrap.com/examples/grid/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-<head>
-<link rel="stylesheet" type="text/css" href="../css/retrieve.css">
-</head>    
+    <title>Grid Template for Bootstrap</title>
 
-<body>
-<?php
-$host="localhost"; // Host name
-$username="root"; // Mysql username
-$password="root"; // Mysql password
-$db_name="at"; // Database name
-$tbl_name="sub1"; // Table name
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/grid.css" rel="stylesheet">
+  </head>
 
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB");
-$myusername = $_GET['username'];
-$sql="SELECT * FROM $tbl_name WHERE teacher=('$myusername')";
-$result=mysql_query($sql);
+  <body>
+  	<?php
+	$host="localhost"; // Host name
+	$username="root"; // Mysql username
+	$password="root"; // Mysql password
+	$db_name="at"; // Database name
+	$tbl_name="sub1"; // Table name
+	mysql_connect("$host", "$username", "$password")or die("cannot connect");
+	mysql_select_db("$db_name")or die("cannot select DB");
+	$myusername = $_GET['username'];
+	$sql="SELECT DISTINCT Subject_code FROM $tbl_name WHERE teacher=('$myusername')";
+	$result=mysql_query($sql);
+	?>
+    <div class="container">
 
-echo "<center>";
-echo "<center>";
-echo "<font size='6' color='white' /><div><h1>Attendance Tracker</h1></div><br \><br \><br \>";
-echo "<table border='1'>";
-echo "<tr>";
-echo "<th><font size=\"5\" color=\"white\">Courses Offered</font></th>";
-echo "</tr>";
+      <div class="page-header">
+        <h1>Attendance Tracker</h1>
+      </div>
 
-while($row = mysql_fetch_array($result))
-  {
-  echo "<tr>";
-  echo "<td><font size='4' color='white'><a href='retrieve_teacher.php?username=$myusername&subcode=".$row['Subject_code']."'>" . $row['Subject_code'] . "</a></font></td>";
-  echo "</tr>"; 
-  }
-?>
-</body>
-</html>
+      <h3>Courses Offered</h3>
+      <div class="row">
+	  <?php
+	  while($row = mysql_fetch_array($result))
+	  {
+	  $sub = $row['Subject_code'];
+	  $url = "retrieve_teacher.php?username=$myusername&subcode=$sub";
+	  echo "<div class=\"col-md-4\"><a href=\"$url\">$sub</a></div>";
+	  }
+	  ?>
+      </div>
+
+    </div>
+
+</body></html>
